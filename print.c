2585d191ce540aa103a69ea17c824dce096a8842
fbasto <fbasto00@gmail.com>
2017-03-26 20:49:23 +0100

    funcoes para imprimir a arvore mas o .h ainda nao esta completo

---
+++ print.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+#include "print.h"
+
+
+
+void print_tree(Node* node,int level){
+	print_points(level);
+	
+	if (node->type == NODE_Id || node->type == NODE_RealLit || node->type == NODE_Declit || node->type == NODE_StrLit){
+		print_leaf(node);
+	}
+	else{
+		printf("%s\n",Node_Name[node->type]);	
+	}
+
+	Node *child = node->child;
+	if (child != NULL){
+		print_tree(child,level+1);
+		while(child->brother != NULL){
+			child = child->brother;
+			print_tree(child,level +1);
+		}
+	}
+
+
+}
+
+
+void print_points(int n){
+	while(n > 0){
+		printf("..");
+		n--;
+	}
+
+
+}
+
+void print_leaf(Node *node){
+	printf("%s(%s)\n", Node_Name[node->type],node->value);
+}
