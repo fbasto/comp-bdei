980e368aae4888ecb0491dfac349e04b7cc1e815
Rui Pedro Ferreira Mendes <ruimendes_10@hotmail.com>
2017-03-28 22:50:07 +0100

    remover merdas inuteis

--- ast.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include "struct.h" 
-
-Node *insert_node(Node_type type);
-Node *insert_leaf_node(Node_type type,char *value);
-void insert_child(Node *father,Node *child);
-void insert_brother(Node *brother,Node *self);
--- print.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "header.h"
-
-//falta um array com o nome dos nos
-void print_tree(Node *node,int level);
-void print_points(int n);
-void print_leaf(Node *node);
