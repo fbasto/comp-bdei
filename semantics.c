08ebcd7175dda921fc27e669427c0318d1e23b1c
Rui Pedro Ferreira Mendes <ruimendes_10@hotmail.com>
2017-04-27 16:21:41 +0100

    semantics - add_vardecl

--- semantics.c
+++ semantics.c
@@ -3,7 +3,16 @@
 #include <string.h>
 #include "printer.h"
 
-extern table *symbol_table;
+static const char *semantic_errors[] = {"Cannot find symbol %s>",
+"Incompatible type %s in <token> statement",
+"Number %s out of bounds",
+"Operator %s cannot be applied to type <type>",
+"Operator %s cannot be applied to types <type>, <type>",
+"Reference to method %s is ambiguous",
+"Symbol %s already defined"};
+
+extern Table *symbol_table;
+Table aux_table = NULL;
 
 void create_symboltable(Node *node){
 	Node* aux = node->child;
@@ -27,4 +36,31 @@ void analyze_node(Node* aux_node){
 	if(strcmp("MethodDecl",aux_name)==0){
 		add_methoddecl(aux_node);
 	}
+}
+
+void add_program(Node* aux_node){ // class gcd2{
+
+}
+
+void add_fielddecl(Node* aux_node){ // class gcd2{ public static int gcd; -- gcd 
+	
+}
+
+void add_vardecl(Node* aux_node){ // int a, b;
+	char* node_name = tree->name; // se houver problemas verificar este print
+	printf("VarDecl node name = %s\n",node_name);
+	char* node_type = tree->type;
+	Symbol *new_symbol = create_symbol(node_name,node_type,0);
+	if(aux_table != NULL){
+		if(search_symbol(aux_table,node_name)==NULL){
+			insert_symbol(aux_table,new_symbol);
+		}
+		else{
+			printf(semantic_errors[6], node_name);
+		}
+	}
+}
+
+void add_methoddecl(Node* aux_node){ // public static int gcd(int a, int b) 
+	
 }
